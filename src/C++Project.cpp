5e9dc4c3942cd4c1c66901f2c65d20ae7b62ad9c
x6tim2plus <503990154@qq.com>
2017-06-15 00:27:12 +0800

    init

---
+++ src/C++Project.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : C++Project.cpp
+// Author      : LCC
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
